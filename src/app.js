4faacce803136d63da75d03c0be3f245ffedf139
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-10-18 15:02:48 +0000

    Import and use expless

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
-const { v4: uuidv4 } = require('uuid');
+const express = require('express');
+const app = express();
+const PORT = 3000;
 
-console.log(uuidv4())
+app.get('/', (req, res) => {
+    res.send('hello word!');
+})
+
+app.post('/', (req, res) => {
+    res.send('This is a post request!');
+})
+
+
+app.listen(PORT, () => {
+    console.log('App listening on port ' + PORT);
+})
